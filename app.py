c1c6d32ca04631ea4331d2ede9fa2b66384da7de
namt93 <namt7115633@gmail.com>
2022-09-08 17:40:12 +0700

    add warning login screen

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 from kivymd.app import MDApp
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import MDList
 
-screen_helper = """
-Screen:
-    BoxLayout:
-        orientation: 'vertical'
-        MDTopAppBar: 
-            title: 'Demo Application'
-        MDLabel:
-            text: 'Hello World'
-            halign: 'center'
+from home_screen import screen_home
 
-"""
+Window.size = (300, 500)
+
 
 class DemoApp(MDApp):
+    class ContentNavigationDrawer(BoxLayout):
+        pass
+
+    class DrawerList(ThemableBehavior, MDList):
+        pass
 
     def build(self):
-        screen = Builder.load_string(screen_helper)
+#        self.theme_cls.primary_palette = 'Pink'
+        self.theme_cls.theme_style = "Dark"
+        screen = Builder.load_string(screen_home)
         return screen
-
+    
+    def navigation_draw(self):
+        print("Navigation")
 
 DemoApp().run()
