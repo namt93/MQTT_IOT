9ba55796f3b8ec003e0bfbb58f7b4856c38768fa
namt93 <namt7115633@gmail.com>
2022-09-10 00:41:27 +0700

    Combine login_screen and home_screen

--- app.py
+++ app.py
@@ -1,33 +1,58 @@
-from kivymd.app import MDApp
-from kivy.uix.widget import Widget
-from kivy.lang import Builder
-from kivymd.uix.label import MDLabel
+from kivymd.app import MDApp, App
 from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.dialog import MDDialog
+from kivy.lang import Builder
+from kivymd.uix.button import MDRectangleFlatButton, MDFlatButton, MDFloatingActionButton
 from kivy.core.window import Window
-from kivy.uix.boxlayout import BoxLayout
 from kivymd.theming import ThemableBehavior
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.widget import Widget
+from kivymd.uix.label import MDLabel
+from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.list import MDList
 
-from home_screen import screen_home
 
 Window.size = (300, 500)
 
 
+class LoginScreen(Screen, MDApp):
+    check_string = ""
+    def login_button_click(self):
+        if self.ids.host_input.text is "":
+            self.check_string = 'Please enter a host'
+        elif self.ids.port_input.text is "": 
+            self.check_string = 'Please enter a port'
+        elif self.ids.webport_input.text is "":
+            self.check_string = 'Please enter a Webport'
+        if self.check_string is "":
+            self.manager.current = 'home'
+        else:
+            close_button = MDFlatButton(text='close', theme_text_color='Custom', text_color=self.theme_cls.primary_color, on_release=self.close_dialog)
+            self.dialog = MDDialog(title='WARNING', text=self.check_string, size_hint=(0.7, 1), buttons=[close_button])
+            self.dialog.open()
+
+    def close_dialog(self, obj):
+        self.check_string = ""
+        self.dialog.dismiss()
+
+
+class HomeScreen(Screen):
+    pass
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+class DrawerList(ThemableBehavior, MDList):
+    pass
+
+
+
+class MQTTScreenManager(ScreenManager):
+    pass
+
 class DemoApp(MDApp):
-    class ContentNavigationDrawer(BoxLayout):
-        pass
-
-    class DrawerList(ThemableBehavior, MDList):
-        pass
-
     def build(self):
-#        self.theme_cls.primary_palette = 'Pink'
-        self.theme_cls.theme_style = "Dark"
-        screen = Builder.load_string(screen_home)
-        return screen
-    
-    def navigation_draw(self):
-        print("Navigation")
-
+        kv_file=Builder.load_file('Screen_manager.kv')
+        return kv_file
 DemoApp().run()
